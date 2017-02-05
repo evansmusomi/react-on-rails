25fb6506f7ffe438acfeb2d8d3389a392b79fb0e
Evans Musomi <evans.musomi@gmail.com>
2017-02-05 15:15:42 +0300

    Formats appointment dates for display using moment.js library

--- app/assets/javascripts/components/appointment.jsx
+++ app/assets/javascripts/components/appointment.jsx
@@ -3,7 +3,7 @@ var Appointment = React.createClass({
         return (
             <div>
                 <h3>{this.props.appointment.title}</h3>
-                <p>{this.props.appointment.appointment_time}</p>
+                <p>{formatDate(this.props.appointment.appointment_time)}</p>
             </div>
         )
     }
